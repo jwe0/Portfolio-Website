d576047325f0267a67934e85e7ee312ecae5930c
jwe0 <jwe0@fbi.gov>
2024-05-25 21:37:37 +0100

    Added a better error page using http cats

---
+++ src/routes/+error.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let status;
+</script>
+
+<main>
+  <img src="https://http.cat/{status}" alt={status} />
+</main>
+
+<style>
+
+  main {
+    background-color: rgb(0, 0, 0);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 98vh;
+  }
+
+   
+</style>
