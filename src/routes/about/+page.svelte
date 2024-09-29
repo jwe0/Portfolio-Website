a75b4a6159672a2b2dab7c98e8064e0128f2b4ba
jwe0 <jwe0@epic.x>
2024-09-29 23:24:20 +0100

    Tidied up the home

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -43,5 +43,23 @@
                 <p>My programming background has honed my problem-solving abilities. I enjoy deconstructing complex problems into manageable parts and often explore multiple solutions to ensure the best outcome.</p>
             </div>
         </div>
-        
+        <p>Here are some of the technologies I have used:</p>
+        <div class="stacks svelte-muvu6j">
+            <img src="https://raw.githubusercontent.com/get-icon/geticon/master/icons/python.svg" width="50" alt="Python">
+            <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" width="50" alt="Node.js"> 
+            <img src="https://raw.githubusercontent.com/get-icon/geticon/master/icons/svelte-icon.svg" width="50" alt="Svelte"> 
+            <img src="https://raw.githubusercontent.com/get-icon/geticon/master/icons/html-5.svg" width="50" alt="HTML"> 
+            <img src="https://raw.githubusercontent.com/get-icon/geticon/master/icons/css-3.svg" width="50" alt="CSS"> 
+            <img src="https://raw.githubusercontent.com/get-icon/geticon/master/icons/javascript.svg" width="50" alt="Javascript"> 
+            <img src="https://raw.githubusercontent.com/get-icon/geticon/master/icons/git.svg" width="130" alt="Git"> 
+            <img style="background-color: white;" src="https://raw.githubusercontent.com/get-icon/geticon/master/icons/flask.svg" width="50" alt="Flask"> 
+            <img style="background-color: white;" src="https://raw.githubusercontent.com/get-icon/geticon/master/icons/rust.svg" width="50" alt="Rust"> 
+            <img src="https://raw.githubusercontent.com/get-icon/geticon/master/icons/linux-tux.svg" width="50" alt="Linux"> 
+            <img src="https://raw.githubusercontent.com/get-icon/geticon/master/icons/ubuntu.svg" width="50" alt="Ubuntu">
+        </div>
+        <p>My GitHub rating</p>
+        <img src="https://github-readme-stats.vercel.app/api?username=jwe0&show_icons=true&theme=tokyonight" alt="GitHub stats">
+        <p>My most used languages</p>
+        <img src="https://github-readme-stats.vercel.app/api/top-langs?username=jwe0&show_icons=true&theme=tokyonight" alt="GitHub stats">
+    </div>
 </body>
