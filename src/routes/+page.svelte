b1eba34746f6627c2b5ab3ce5beacb7e50e181f8
Jwe0 <jwe0@jwe0.xyz>
2024-10-20 22:08:08 +0100

    Updated home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,10 +26,30 @@
         </div>
     
         <div class="main">
-            <h1>Joshua Webb</h1>
-            <p>16 year old software engineer</p>
-            <h5>"that's how you turn a 5 hour task into a 1 and a half month task, because i'm a programmer, and that's what we do."</h5>
+            <div class="home">
+                <h1>Joshua Webb</h1>
+                <div class="aboutme_home">
+                    <div class="box">
+                        <h1 class="title">Whoami</h1>
+                        <p>I am a tech enthusiast. I enjoy programming, networking and security. I have 4+ years of experience in programming as well as 2 years of experience in networking and security.</p>
+                    </div>
+                    <div class="box">
+                        <h1 class="title">Qualifications</h1>
+                        <p>I have completed a BTEC Digital Information Technology course at GCSE level and achieved a merit overall. I am currently doing a Digital Skills & IT Intermediate at College and intend to do a Level 3 T-Level or an Apprenticeship.</p>
+                    </div>  
+                    <div class="box">
+                        <h1 class="title">Work experience</h1>
+                        <p>I am currently in a volunteering role at the Air Ambulance charity shop in my town, this has allowed me to get customer service skills and how to work on a till.</p>
+                    </div>      
+                    <div class="box">
+                        <h1 class="title">Hobbies</h1>
+                        <p>Besides my technical interest I am a fan of climbing both in gyms and outside in trees.</p>
+                    </div> 
+                </div>
+            </div>
+
         </div>
+
     </div>
 
 </body>
